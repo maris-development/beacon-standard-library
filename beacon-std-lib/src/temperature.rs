7d8d0f1feb60e293791be45d3db569b403b7904b
Robin Kooyman <robin.kooyman.work@gmail.com>
2024-04-03 16:49:04 +0200

    simplify library

--- beacon-std-lib/src/temperature.rs
+++ beacon-std-lib/src/temperature.rs
@@ -1,82 +1,7 @@
-use std::collections::HashMap;
-
-use lazy_static::lazy_static;
-
-macro_rules! register_std_lib_conversions {
-    ($path:ident,$($name:ident => $func:expr , $func_inverse: expr),*) => {
-    lazy_static! {
-            pub static ref STD_CONVERSIONS:  HashMap<&'static str, Conversion<f64>> = {
-                let mut m = HashMap::new();
-
-                $(
-                    m.insert(concat!("std::",stringify!($path),"::",stringify!($name)), Conversion { conversion: $func as fn(f64) -> f64 , inverse_conversion: $func_inverse as fn(f64) -> f64 });
-                )*
-
-                m
-            };
-        }
-    };
-}
-
-pub struct Conversion<T> {
-    conversion: fn(T) -> T,
-    inverse_conversion: fn(T) -> T,
-}
-
-impl<T> Conversion<T> {
-    pub fn conversion_fn(&self) -> fn(T) -> T {
-        self.conversion
-    }
-
-    pub fn inverse_conversion_fn(&self) -> fn(T) -> T {
-        self.inverse_conversion
-    }
-}
-
-register_std_lib_conversions! {
-    conversions, 
-    
-    kelvin_to_celsius => kelvin_to_celsius, celsius_to_kelvin, 
-    
-    celsius_to_kelvin => celsius_to_kelvin, kelvin_to_celsius,
-    
-    kelvin_to_fahrenheit => kelvin_to_fahrenheit, fahrenheit_to_kelvin,
-
-    fahrenheit_to_kelvin => fahrenheit_to_kelvin, kelvin_to_fahrenheit, 
-    
-    celsius_to_fahrenheit => celsius_to_fahrenheit , fahrenheit_to_celsius,
-
-    fahrenheit_to_celsius => fahrenheit_to_celsius, celsius_to_fahrenheit,
-
-    empty_conversion => empty_conversion, empty_conversion
-}
-
-/// This function returns the input value `x` unchanged.
-/// 
-/// # Method name in Beacon Query:
-/// 
-/// ### `std::conversions::empty_conversion`
-///
-/// # Arguments
-///
-/// * `x` - A floating point number.
-///
-/// # Example
-///
-/// ```
-/// use beacon_std_lib::conversions::empty_conversion;
-///
-/// let x = 3.14;
-/// assert_eq!(empty_conversion(x), x);
-/// ```
-pub fn empty_conversion(x: f64) -> f64 {
-    x
-}
-
 /// Converts temperature in Celsius to Kelvin.
-/// 
+///
 /// # Method name in Beacon Query:
-/// 
+///
 /// ### `std::conversions::celsius_to_kelvin`
 ///
 /// # Arguments
@@ -92,15 +17,15 @@ pub fn empty_conversion(x: f64) -> f64 {
 /// let kelvin = celsius_to_kelvin(celsius);
 /// assert_eq!(kelvin, 298.15);
 /// ```
-/// 
+///
 pub fn celsius_to_kelvin(x: f64) -> f64 {
     x + 273.15
 }
 
 /// Converts temperature in Celsius to Fahrenheit.
-/// 
+///
 /// # Method name in Beacon Query:
-/// 
+///
 /// ### `std::conversions::celsius_to_fahrenheit`
 ///
 /// # Arguments
@@ -121,9 +46,9 @@ pub fn celsius_to_fahrenheit(x: f64) -> f64 {
 }
 
 /// Converts temperature in Kelvin to Celsius.
-/// 
+///
 /// # Method name in Beacon Query:
-/// 
+///
 /// ### `std::conversions::kelvin_to_celsius`
 ///
 /// # Arguments
@@ -144,9 +69,9 @@ pub fn kelvin_to_celsius(x: f64) -> f64 {
 }
 
 /// Converts temperature in Kelvin to Fahrenheit.
-/// 
+///
 /// # Method name in Beacon Query:
-/// 
+///
 /// ### `std::conversions::kelvin_to_fahrenheit`
 ///
 /// # Arguments
@@ -157,7 +82,7 @@ pub fn kelvin_to_celsius(x: f64) -> f64 {
 ///
 /// ```
 /// use beacon_std_lib::conversions::kelvin_to_fahrenheit;
-/// 
+///
 /// let fahrenheit = kelvin_to_fahrenheit(275.15);
 /// assert_eq!(fahrenheit, 35.6);
 /// ```
@@ -166,9 +91,9 @@ pub fn kelvin_to_fahrenheit(x: f64) -> f64 {
 }
 
 /// Converts a temperature value from Fahrenheit to Kelvin.
-/// 
+///
 /// # Method name in Beacon Query:
-/// 
+///
 /// ### `std::conversions::fahrenheit_to_kelvin`
 ///
 /// # Arguments
@@ -179,7 +104,7 @@ pub fn kelvin_to_fahrenheit(x: f64) -> f64 {
 ///
 /// ```
 /// use beacon_std_lib::conversions::fahrenheit_to_kelvin;
-/// 
+///
 /// let kelvin = fahrenheit_to_kelvin(32.0);
 /// assert_eq!(kelvin, 273.15);
 /// ```
@@ -188,9 +113,9 @@ pub fn fahrenheit_to_kelvin(x: f64) -> f64 {
 }
 
 /// Converts a temperature value from Fahrenheit to Celsius.
-/// 
+///
 /// # Method name in Beacon Query:
-/// 
+///
 /// ### `std::conversions::fahrenheit_to_celsius`
 ///
 /// # Arguments
@@ -209,20 +134,4 @@ pub fn fahrenheit_to_kelvin(x: f64) -> f64 {
 /// ```
 pub fn fahrenheit_to_celsius(x: f64) -> f64 {
     (x - 32.0) * 5.0 / 9.0
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_lib_contains() {
-        assert!(STD_CONVERSIONS.contains_key("std::conversions::celsius_to_kelvin"));
-        assert!(STD_CONVERSIONS.contains_key("std::conversions::kelvin_to_celsius"));
-        assert!(STD_CONVERSIONS.contains_key("std::conversions::celsius_to_fahrenheit"));
-        assert!(STD_CONVERSIONS.contains_key("std::conversions::fahrenheit_to_celsius"));
-        assert!(STD_CONVERSIONS.contains_key("std::conversions::kelvin_to_fahrenheit"));
-        assert!(STD_CONVERSIONS.contains_key("std::conversions::fahrenheit_to_kelvin"));
-        assert!(STD_CONVERSIONS.contains_key("std::conversions::empty_conversion"));
-    }
 }
--- beacon-std-lib/src/time.rs
+++ beacon-std-lib/src/time.rs
@@ -1,36 +1,21 @@
+use std::str::FromStr;
+
 /// This module contains functions related to time manipulation.
 /// It provides conversions between different time representations and utilities for calculating time intervals.
 /// The functions in this module are used in the Beacon Standard Library.
 use hifitime::Duration;
-use lazy_static::lazy_static;
-use std::{collections::HashMap, ops::Deref, str::FromStr};
-
-use crate::args::Args;
-
-/// A method that takes a single argument of type `T` and an `Args` struct and returns a result of type `anyhow::Result<T>`.
-#[derive(Clone, Copy)]
-pub struct ArgsMethod<T>(pub fn(T, Args) -> anyhow::Result<T>);
-
-impl Deref for ArgsMethod<f64> {
-    type Target = fn(f64, Args) -> anyhow::Result<f64>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
 
 /// Converts Julian Ephemeris Date (JDE) to Unix timestamp.
 ///
 /// # Arguments
 ///
 /// * `jde` - The Julian Ephemeris Date to convert.
-/// * `_` - An `Args` struct that is not used in this function.
 ///
 /// # Returns
 ///
 /// The Unix timestamp corresponding to the given JDE.
 #[inline]
-pub fn jde_to_unix(jde: f64, _: Args) -> anyhow::Result<f64> {
+pub fn jde_to_unix(jde: f64) -> anyhow::Result<f64> {
     let epoch = hifitime::Epoch::from_jde_utc(jde);
     Ok(epoch.to_unix_seconds())
 }
@@ -39,52 +24,30 @@ pub fn jde_to_unix(jde: f64, _: Args) -> anyhow::Result<f64> {
 ///
 /// # Arguments
 ///
-/// * `value` - The number of seconds since the epoch.
-/// * `args` - An `Args` struct that contains the epoch argument.
+/// * `seconds` - The number of seconds since the epoch.
+/// * `epoch` - The epoch to use as a reference.
 ///
 /// # Returns
 ///
 /// The Unix timestamp corresponding to the given number of seconds since the epoch.
-pub fn seconds_since_epoch_to_unix(value: f64, args: Args) -> anyhow::Result<f64> {
-    let epoch_arg = args.arg("epoch")?;
-    let epoch = hifitime::Epoch::from_str(epoch_arg.as_str()?)?;
-    Ok((epoch + Duration::from_seconds(value)).to_unix_seconds())
+#[inline]
+pub fn seconds_since_epoch_to_unix(seconds: f64, epoch: &str) -> anyhow::Result<f64> {
+    let epoch = hifitime::Epoch::from_str(epoch)?;
+    Ok((epoch + Duration::from_seconds(seconds)).to_unix_seconds())
 }
 
 /// Converts days since a given epoch to Unix timestamp.
 ///
 /// # Arguments
 ///
-/// * `value` - The number of days since the epoch.
-/// * `args` - An `Args` struct that contains the epoch argument.
+/// * `days` - The number of days since the epoch.
+/// * `epoch` - The epoch to use for the conversion.
 ///
 /// # Returns
 ///
 /// The Unix timestamp corresponding to the given number of days since the epoch.
-pub fn days_since_epoch_to_unix(value: f64, args: Args) -> anyhow::Result<f64> {
-    let epoch_arg = args.arg("epoch")?;
-    let epoch = hifitime::Epoch::from_str(epoch_arg.as_str()?)?;
-    Ok((epoch + Duration::from_days(value)).to_unix_seconds())
+#[inline]
+pub fn days_since_epoch_to_unix(days: f64, epoch: &str) -> anyhow::Result<f64> {
+    let epoch = hifitime::Epoch::from_str(epoch)?;
+    Ok((epoch + Duration::from_days(days)).to_unix_seconds())
 }
-
-macro_rules! register_args_function {
-    ($($name:ident => $func:expr),*) => {
-    lazy_static! {
-            pub static ref ARGS_FUNCTIONS:  HashMap<&'static str, ArgsMethod<f64>> = {
-                let mut m = HashMap::new();
-
-                $(
-                    m.insert(concat!(stringify!($name)), ArgsMethod ($func as fn(f64, Args) -> anyhow::Result<f64>));
-                )*
-
-                m
-            };
-        }
-    };
-}
-
-register_args_function!(
-    jde_to_unix => jde_to_unix,
-    seconds_since_epoch_to_unix => seconds_since_epoch_to_unix,
-    days_since_epoch_to_unix => days_since_epoch_to_unix
-);
